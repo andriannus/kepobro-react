00db079f0b3ccd9ffa7942eab78f46e3d58ae501
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2019-08-24 17:44:39 +0700

    refactor - update left sidebar

--- src/shared/components/sidebar-left/sidebar-left.js
+++ src/shared/components/sidebar-left/sidebar-left.js
@@ -2,15 +2,55 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import { CATEGORIES } from 'shared/constants/news.constant';
+import 'shared/components/sidebar-left/sidebar-left.scss';
 
 export const Container = () => {
   return (
     <div className="column">
-      <div className="card is-hidden-mobile sidebar">
-        <CardHeader />
+      <Mobile />
 
-        <CardContent />
+      <Desktop />
+    </div>
+  )
+}
+
+const Mobile = () => {
+  return (
+    <div className="bra-3 has-background-white">
+      <div
+        className="tabs is-toggle is-hidden-tablet"
+      >
+        <ul>
+          {
+            CATEGORIES.map((category, key) => {
+              if (category === 'trending') return null;
+
+              return(
+                <li key={key}>
+                  <NavLink
+                    activeClassName="is-active"
+                    to={`/${category}`}
+                  >
+                    <span className="is-capitalized">
+                      {category}
+                    </span>
+                  </NavLink>
+                </li>
+              )
+            })
+          }
+        </ul>
       </div>
+    </div>
+  )
+}
+
+const Desktop = () => {
+  return (
+    <div className="card is-hidden-mobile sidebar">
+      <CardHeader />
+
+      <CardContent />
     </div>
   )
 }
